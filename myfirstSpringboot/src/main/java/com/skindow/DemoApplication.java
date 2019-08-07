5ed3f38ec061df37acc08e1eaabc8e6e54ea221d
skindowSyc <295317224@qq.com>
2019-08-07 09:44:02 +0800

    修复datasource初始化的错误，解决方案禁止初始化datasource

--- myfirstSpringboot/src/main/java/com/skindow/DemoApplication.java
+++ myfirstSpringboot/src/main/java/com/skindow/DemoApplication.java
@@ -2,8 +2,10 @@ package com.skindow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+//避免Failed to determine a suitable driver class错误 由于没有使用到datasource 所以就不用初始化datasource了
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {

277ca02824fd98e8a564fff557e564a8e8d8ab54
skindowSyc <295317224@qq.com>
2019-09-04 10:23:53 +0800

    优化Log输出

--- myfirstSpringboot/skindow-parent/skindow-consumer/src/main/java/com/skindow/logAop/LogAspect.java
+++ myfirstSpringboot/skindow-parent/skindow-consumer/src/main/java/com/skindow/logAop/LogAspect.java
@@ -8,7 +8,10 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.annotation.Annotation;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +40,10 @@ public class LogAspect {
         //返回目标方法所有的参数
         Object[] args = joinPoint.getArgs();
         log.info("{} 调用时间：{}", signature, sdf.format(startDate));
+        executorService.execute(()->
+        {
+           log.info("{} 方法入参 {}", signature.toString(),new ArrayList<>(Arrays.asList(args)).stream().map(a -> logString(a)).collect(Collectors.joining(",")));
+        });
         long start = System.currentTimeMillis();
         //用改变后的参数执行目标方法
         Object object = joinPoint.proceed();
@@ -44,14 +51,44 @@ public class LogAspect {
         long end = System.currentTimeMillis();
         String time = this.formatExecuteTime(end - start);
         log.info("{} 执行时间：{}", signature.toString(), time);
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                log.info("{} 方法入参 {}", signature.toString(),new ArrayList<>(Arrays.asList(args)).stream().map(a -> JSON.toJSONString(a)).collect(Collectors.joining(",")));
-                log.info("{} 方法出参 {}",signature.toString(), object);
+        executorService.execute(() -> {
+            if ( isReseponse(signature))
+            {
+                log.info("{} 方法出参 {}",signature.toString(), JSON.toJSON(object));
+            }
+            else
+            {
+                log.info("{} 跳转页面到 {}",signature.toString(), JSON.toJSON(object));
             }
         });
         return object;
+    }
+
+    private boolean isReseponse(Signature signature)
+    {
+        Annotation[] annotations = signature.getDeclaringType().getDeclaredAnnotations();
+        for (Annotation annotation : annotations)
+        {
+            boolean annotationPresent = annotation.annotationType().isAnnotationPresent(ResponseBody.class);
+            if (annotationPresent)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String logString(Object o)
+    {
+        if (o.getClass().isInterface())
+        {
+            return o.toString();
+        }
+        if (o instanceof Model)
+        {
+            return null;
+        }
+        return "【" + o.getClass().getName() + "】toJSON " + JSON.toJSONString(o);
     }
 
     /**通过毫秒计算出时分秒并输出xxmxxsxxms的格式字符

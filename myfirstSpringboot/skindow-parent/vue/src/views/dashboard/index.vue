36034060a4978e20798585e35eea370971826e9a
skindow <295317224@qq.com>
2019-09-20 10:51:35 +0800

    auth update url

--- myfirstSpringboot/skindow-parent/vue/src/views/dashboard/index.vue
+++ myfirstSpringboot/skindow-parent/vue/src/views/dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank"><img style="margin: 40px"
+    <a href="https://github.com/skindowSyc/skindow" target="_blank"><img style="margin: 40px"
       src="http://img.heeexy.com/github.png"></a>
   </div>
 </template>
